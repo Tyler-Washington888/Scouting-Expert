491106245ee6c8d70716371dd2f8da2bb4eab30f
Tyler washington <lilturtleneckbeats@gmail.com>
2022-11-19 22:39:44 -0500

    heroku push 6

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`.yellow)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.underline)
     }catch(e){
         console.log(e)
         process.exit(1);
